1d8beb6bc1fdba5d6cad5465a28b75385eedeb7b
Stanislau_Hramatskiu <Stanislau_Hramatskiu@epam.com>
2020-02-03 16:52:08 +0300

    Initial commit

--- cmd/import-job.sh
+++ cmd/import-job.sh
@@ -1,5 +1,6 @@
 PASSWORD=$(cat /var/jenkins_home/secrets/initialAdminPassword)
 echo $PASSWORD
+#If CRSF disabled this header is unnecessary
 #CRUMB=$(curl --silent 'http://localhost:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)' -u admin:$PASSWORD)
 #echo $CRUMB
 curl --silent -H "Content-Type:text/xml" -XPOST 'http://localhost:8080/createItem?name=jn-task' -u admin:$PASSWORD --data-binary @/cmd/task.xml
