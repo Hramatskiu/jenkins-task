e16700dda7a2f2ce8d003c0312663e962d89291b
Stanislau_Hramatskiu <Stanislau_Hramatskiu@epam.com>
2020-02-04 14:20:51 +0300

    Initial commit

--- run.cmd
+++ run.cmd
@@ -14,7 +14,7 @@ ECHO Import pipeline
 docker cp cmd jenkins:/ >> %OUT_LOG_LOCATION% 2>> %ERR_LOG_LOCATION%
 docker exec -ti -u root jenkins /bin/chmod -R 777 /cmd >> %OUT_LOG_LOCATION% 2>> %ERR_LOG_LOCATION%
 docker exec -ti jenkins /bin/bash /cmd/import-job.sh >> %OUT_LOG_LOCATION% 2>> %ERR_LOG_LOCATION%
-ECHO View pipeline on <DOCKER_IP>:48080/job/jn-task/
+ECHO View pipeline on 48080 port
 ECHO Use load-jn-task-logs.cmd to load pipeline logs
 ECHO Press Enter to stop and delete jenkins container
 PAUSE
